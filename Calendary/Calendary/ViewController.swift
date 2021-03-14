4725171034d15546cb0cfd277a0a9f6e9d6beb8d
key5771 <ksj57715@hanmail.net>
2021-03-14 15:45:12 +0900

    Add Library

--- Calendary/Calendary/ViewController.swift
+++ Calendary/Calendary/ViewController.swift
@@ -11,9 +11,12 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        print("ViewDidLoad")
     }
 
-
+    override func viewWillAppear(_ animated: Bool) {
+        print("ViewWillAppear")
+    }
 }
 
